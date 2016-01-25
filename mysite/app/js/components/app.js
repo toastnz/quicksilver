14f1053d9b00f7095f680fde584c679e4f7be2cd
Jaydn de Graaf <jd@pinc.nz>
2016-01-26 10:45:05 +1300

    Deprecated code

--- mysite/app/js/components/app.js
+++ mysite/app/js/components/app.js
@@ -4,21 +4,19 @@ window.jQuery = window.$ = require('jquery');
 // Requirements
 // ======================================================
 
-const $             = require('jquery');
-const _             = require('underscore');
-const fancybox      = require('fancybox')($);
-const imagesLoaded  = require('imagesloaded');
-const slick         = require('slick-carousel');
-const jQBridget     = require('jquery-bridget');
-const Masonry       = require('masonry-layout');
-const  matchHeight  = require('jquery-match-height');
+const $ = require('jquery');
+const _ = require('underscore');
+const fancybox = require('fancybox')($);
+const imagesLoaded = require('imagesloaded');
+const slick = require('slick-carousel');
+const jQBridget = require('jquery-bridget');
+const Masonry = require('masonry-layout');
+const matchHeight = require('jquery-match-height');
 
 // ======================================================
 // Components
 // ======================================================
 
-const template    = require('./templates');
-const selectOrDie = require('./selectordie');
 const snappy = require('../snappy/snappy');
 
 
