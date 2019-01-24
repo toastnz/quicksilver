0aae0a42040ab142cfec83cee29593d4ea8843d8
Jaydn de Graaf <jd@pinc.nz>
2019-01-24 15:50:19 +1300

    Updated the style guide controller

--- mysite/code/controllers/StyleGuideController.php
+++ mysite/code/controllers/StyleGuideController.php
@@ -7,35 +7,17 @@ use SilverStripe\Security\Security;
 class StyleGuideController extends Controller
 {
 
-    private static $allowed_actions = array(
+    private static $allowed_actions = [
         'index'
-    );
+    ];
 
     public function index()
     {
-        $member = Security::getCurrentUser();
-       // var_dump($member);die;
-        if (is_null($member)) {
-            $backUrl = $this->owner->getRequest()->getVar('BackURL');
-                if (Controller::curr()->class != 'Account') {
-                    $link = 'Security/login' . '?BackURL=' . urlencode($backUrl);
-                    return $this->owner->redirect($link);
-                }
+        if (is_null(Security::getCurrentUser())) {
+            return $this->redirect('Security/login?BackURL=_styleguide');
         } else {
-            //return $this->owner->renderWith (['StyleGuide', 'Page']);
-            return $this->customise($member)->renderWith(
-                ['StyleGuideController', 'Page']
-            );
-       //     $this->redirect('StyleGuide');
-//            return $this->owner->renderWith(
-//                ['StyleGuide', 'Page']
-//            );
-          //  $this->owner->template['index'] = array('StyleGuide', 'Page');
-          //  return $this->renderWith('StyleGuide','Page');
+            return $this->renderWith(['StyleGuideController', 'Page']);
         }
     }
-//    function Template()
-//    {
-//        return $this->owner->renderWith([$this->owner->ClassName, 'Includes/StyleGuide']);
-//    }
+
 }
