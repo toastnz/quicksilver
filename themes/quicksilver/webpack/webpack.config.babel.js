d691595c0ae966ac57aea87dbd7b1b1905fb9256
Jaydn de Graaf <jd@pinc.nz>
2019-06-21 10:07:31 +1200

    update browserlistrc upgrade yarn

--- themes/quicksilver/webpack/webpack.config.babel.js
+++ themes/quicksilver/webpack/webpack.config.babel.js
@@ -1,28 +1,28 @@
-const path                        = require('path');
-const webpack                     = require('webpack');
-const autoprefixer                = require('autoprefixer');
-const SpritesmithPlugin           = require('webpack-spritesmith');
+const path = require('path');
+const webpack = require('webpack');
+const autoprefixer = require('autoprefixer');
+const SpritesmithPlugin = require('webpack-spritesmith');
 const { getIfUtils, removeEmpty } = require('webpack-config-utils');
-const MiniCssExtractPlugin        = require('mini-css-extract-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const FriendlyErrorsWebpackPlugin = require('friendly-errors-webpack-plugin');
-const OptimizeCssAssetsPlugin     = require('optimize-css-assets-webpack-plugin');
-const UglifyJsPlugin              = require("uglifyjs-webpack-plugin");
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
 
 const stats = {
-    colors      : true,
-    hash        : false,
-    version     : false,
-    timings     : false,
-    assets      : false,
-    chunks      : false,
-    modules     : false,
-    reasons     : false,
-    children    : false,
-    source      : false,
-    errors      : false,
+    colors: true,
+    hash: false,
+    version: false,
+    timings: false,
+    assets: false,
+    chunks: false,
+    modules: false,
+    reasons: false,
+    children: false,
+    source: false,
+    errors: false,
     errorDetails: false,
-    warnings    : false,
-    publicPath  : false,
+    warnings: false,
+    publicPath: false,
 };
 
 module.exports = (env, argv) => {
@@ -30,68 +30,59 @@ module.exports = (env, argv) => {
     const { ifProduction } = getIfUtils(argv.mode);
 
     return {
-        mode        : ifProduction('production', 'development'),
-        entry       : path.resolve(__dirname, '../js/app.js'),
+        mode: ifProduction('production', 'development'),
+        entry: path.resolve(__dirname, '../js/app.js'),
         stats,
-        devtool     : ifProduction('eval', 'source-map'),
-        output      : {
-            path             : path.resolve(__dirname, '../dist/scripts'),
-            filename         : 'bundle.js',
+        devtool: ifProduction('', 'source-map'),
+        output: {
+            path: path.resolve(__dirname, '../dist/scripts'),
+            filename: 'bundle.js',
             sourceMapFilename: 'bundle.map.js'
         },
-        module      : {
+        module: {
             rules: [
                 {
-                    test   : /\.js$/,
+                    test: /\.js$/,
                     exclude: /node_modules/,
-                    loader : 'babel-loader'
+                    loader: 'babel-loader'
                 },
                 {
                     test: /\.scss$/,
-                    use : [
+                    use: [
                         MiniCssExtractPlugin.loader,
                         {
-                            loader : 'css-loader',
+                            loader: 'css-loader',
                             options: { sourceMap: ifProduction(true, false) }
                         },
                         {
-                            loader : 'postcss-loader',
+                            loader: 'postcss-loader',
                             options: {
                                 sourceMap: ifProduction(true, false),
-                                plugins  : [
-                                    autoprefixer({
-                                        browsers: [
-                                            'ie >= 9',
-                                            'ie_mob >= 11',
-                                            'Safari >= 10',
-                                            'Android >= 4.4',
-                                            'Chrome >= 44',
-                                            'Samsung >= 4'
-                                        ]
-                                    })
+                                plugins: [
+                                    autoprefixer()
                                 ]
                             }
                         },
                         {
-                            loader : 'sass-loader',
+                            loader: 'sass-loader',
                             options: { sourceMap: ifProduction(true, false) }
                         },
                         {
-                            loader : 'import-glob-loader',
+                            loader: 'import-glob-loader',
                             options: { sourceMap: ifProduction(true, false) }
                         },
                     ]
                 },
                 {
-                    test   : /\.(jpg|png|gif|svg)$/,
-                    loader : 'image-webpack-loader',
+                    test: /\.(jpg|png|gif|svg)$/,
+                    loader: 'image-webpack-loader',
                     enforce: 'pre'
                 },
                 {
-                    test   : /\.(png|jpg|gif|eot|ttf|woff|woff2)$/,
-                    loader : 'file-loader',
+                    test: /\.(png|jpg|gif|eot|ttf|woff|woff2)$/,
+                    loader: 'file-loader',
                     options: {
-                        name      : '[name][hash].[ext]',
+                        name: '[name][hash].[ext]',
                         outputPath: '../images/generated/'
                     }
                 },
@@ -100,41 +91,41 @@ module.exports = (env, argv) => {
         optimization: {
             minimizer: [
                 new UglifyJsPlugin({
-                    cache    : true,
-                    parallel : true,
+                    cache: true,
+                    parallel: true,
                     sourceMap: ifProduction(true, false)
                 }),
                 new OptimizeCssAssetsPlugin({
                     cssProcessorOptions: { discardComments: { removeAll: true } },
-                    canPrint           : true
+                    canPrint: true
                 })
             ]
         },
-        plugins     : removeEmpty([
+        plugins: removeEmpty([
             new FriendlyErrorsWebpackPlugin(),
             new webpack.PrefetchPlugin(path.resolve(__dirname, '../scss/style.scss')),
             new MiniCssExtractPlugin({
-                filename     : '../styles/[name].css',
+                filename: '../styles/[name].css',
                 chunkFilename: '[id].css'
             }),
             new SpritesmithPlugin({
-                src               : {
-                    cwd : path.resolve(__dirname, '../sprites'),
+                src: {
+                    cwd: path.resolve(__dirname, '../sprites'),
                     glob: '*.png'
                 },
-                target            : {
+                target: {
                     image: path.resolve(__dirname, '../dist/images/sprites/sprite.png'),
-                    css  : [
+                    css: [
                         [path.resolve(__dirname, '../spritesmith-generated/_normal.scss'), { format: 'normal' }],
                         [path.resolve(__dirname, '../spritesmith-generated/_retina.scss'), { format: 'retina' }]
                     ]
                 },
-                retina            : '@2x',
-                apiOptions        : { cssImageRef: '~sprite.png' },
-                customTemplates   : {
-                    normal       : path.resolve(__dirname, '../sprites/sprite_positions.styl.mustache'),
+                retina: '@2x',
+                apiOptions: { cssImageRef: '~sprite.png' },
+                customTemplates: {
+                    normal: path.resolve(__dirname, '../sprites/sprite_positions.styl.mustache'),
                     normal_retina: path.resolve(__dirname, '../sprites/sprite_positions.styl.mustache'),
-                    retina       : path.resolve(__dirname, '../sprites/retina-sprite_positions.styl.mustache'),
+                    retina: path.resolve(__dirname, '../sprites/retina-sprite_positions.styl.mustache'),
                     retina_retina: path.resolve(__dirname, '../sprites/retina-sprite_positions.styl.mustache'),
                 },
                 spritesmithOptions: { padding: 4 }
