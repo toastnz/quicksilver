df79b03b19168ab5fbdcb9c898908421e3657e7e
Delma <delma@toast.co.nz>
2019-10-16 17:03:27 +1300

    Banner slider

--- mysite/code/Page.php
+++ mysite/code/Page.php
@@ -1,15 +1,77 @@
 <?php
 
+use Sheadawson\Linkable\Forms\LinkField;
+use Sheadawson\Linkable\Models\Link;
+use SilverStripe\AssetAdmin\Forms\UploadField;
+use SilverStripe\Assets\Image;
 use SilverStripe\Control\Director;
 use SilverStripe\CMS\Model\SiteTree;
+use SilverStripe\Forms\GridField\GridField;
+use SilverStripe\Forms\GridField\GridFieldConfig_RelationEditor;
+use SilverStripe\Forms\GridField\GridFieldDeleteAction;
+use SilverStripe\Forms\TextareaField;
+use SilverStripe\Forms\TextField;
+use Symbiote\GridFieldExtensions\GridFieldOrderableRows;
 
 /**
  * Class Page
  *
  * @property string Theme
  */
+
+
 class Page extends SiteTree
 {
+    private static $db = [
+        'BannerTitle'        => 'Text',
+    ];
+    private static $has_one = [
+        'BannerImage' => Image::class,
+        'BannerLink'  => Link::class,
+    ];
+    private static $has_many = [
+        'BannerSliders' => BannerSlider::class
+    ];
+
+    private static $owns = [
+        'BannerImage',
+    ];
+
+    public function getCMSFields()
+    {
+        $sliderConfig = GridFieldConfig_RelationEditor::create(10);
+        $sliderConfig->addComponent(GridFieldOrderableRows::create('SortOrder'))
+            ->removeComponentsByType(GridFieldDeleteAction::class)
+            ->addComponent(new GridFieldDeleteAction(false));
+        $sliderGridfield = GridField::create(
+            'BannerSliders',
+            'Banner',
+            $this->BannerSliders(),
+            $sliderConfig
+        );
+        $fields = parent::getCMSFields();
+        /** -----------------------------------------
+         * Banner
+         * ----------------------------------------*/
+        $fields->addFieldsToTab('Root.Banner', [
+            TextField::create('BannerTitle', 'Banner Title'),
+            LinkField::create('BannerLinkID', 'Banner Link')
+        ]);
+        if ($this->ClassName == \Toast\Pages\HomePage::class) {
+            $fields->removeByName(['BannerImage']);
+            $fields->addFieldsToTab('Root.Banner', [
+                $sliderGridfield
+            ]);
+        } else {
+            $fields->removeByName(['BannerSliders']);
+            $fields->addFieldsToTab('Root.Banner', [
+                UploadField::create('BannerImage', 'Banner Image')
+                    ->setFolderName('Uploads/BannerImage')
+                    ->setDescription('Displays Banner Image (jpeg,jpg,png)')
+            ]);
+        }
+        return $fields;
+    }
     public function getIsLive()
     {
         return Director::isLive();
